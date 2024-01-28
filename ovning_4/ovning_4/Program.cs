7adeba4af09834e946c2cd5fbae1a98591f74974
Teo Ingerman <teo.ingerman@elev.ga.ntig.se>
2024-01-28 23:50:05 +0100

    övning 4 added

---
+++ ovning_4/ovning_4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ovning_4
+{
+    internal class Program
+    {
+
+        static bool password_check(string password)
+        {
+            
+            Console.Write("password: ");
+            string input_password = null;
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                Console.Write("*");
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+                input_password += key.KeyChar;
+            }
+            Console.Write("\n");
+            if (input_password == password)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            
+            
+            while (true)
+            {
+                bool permission = password_check("hello");
+
+                if (permission == true)
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+
+
+            int age = 10;
+            int price;
+
+
+
+            
+        }
+    }
+}
