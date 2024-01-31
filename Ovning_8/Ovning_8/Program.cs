7e5c9b5da0585afb214f4ad8eb124ff7e45c914a
Teo Ingerman <teo.ingerman@elev.ga.ntig.se>
2024-01-31 10:07:37 +0100

    yes

---
+++ Ovning_8/Ovning_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        // uppgift 1
+        static void tv_printout(string name, string sum)
+        {
+            Console.WriteLine($"{name} har donerat {sum} kronor");
+        }
+
+        // tid över
+        static void circle_area_calc(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"area is equal to {area}");
+            // return area; is probably better 
+        }
+
+    }
+}
--- ovning_9/ovning_9/Program.cs
+++ ovning_9/ovning_9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,69 @@ namespace ovning_9
     {
         static void Main(string[] args)
         {
-            
-            
+
+            Console.WriteLine(nice_word_finder('p'));
                     
         }
+    
+           
+        static double degree_converter(double degree, char system_from  )
+        {
+            double new_degree = 0;
+
+            if (system_from == 'F')
+            {
+                new_degree = (degree-32)*(5/9);
+            }
+            if (system_from == 'C')
+            {
+                new_degree = (degree * (9/5)) - 32;
+            }
+            return new_degree;
+        }
+
+        
+
+        static string nice_word_finder(char letter)
+        {
+            string nice_word;
+
+            Random rnd = new Random();
+
+            List<string> word_list = new List<string>();
+
+            String line;
+            try
+            {
+                StreamReader sr = new StreamReader("nice_words.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    word_list.Add(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            string[] resultWords = word_list.Where(word => word.StartsWith(letter.ToString(), StringComparison.OrdinalIgnoreCase)).ToArray();
+
+            int word_index = rnd.Next(0, resultWords.Count() - 1);
+
+            nice_word = resultWords[word_index];
+
+            return nice_word;
+
+        }
+
+
+    
     }
 }
