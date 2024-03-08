9837692291a883cb143d0a0d35fb163032c177aa
Teo Ingerman <teo.ingerman@elev.ga.ntig.se>
2024-03-08 10:20:02 +0100

    yes

---
+++ Grafiska_program/uppgift_8_1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace uppgift_8_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_left = new System.Windows.Forms.Button();
+            this.button_right = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_left
+            // 
+            this.button_left.Location = new System.Drawing.Point(250, 108);
+            this.button_left.Name = "button_left";
+            this.button_left.Size = new System.Drawing.Size(75, 23);
+            this.button_left.TabIndex = 0;
+            this.button_left.Text = "vänster";
+            this.button_left.UseVisualStyleBackColor = true;
+            this.button_left.Click += new System.EventHandler(this.button_left_Click);
+            // 
+            // button_right
+            // 
+            this.button_right.Location = new System.Drawing.Point(387, 107);
+            this.button_right.Name = "button_right";
+            this.button_right.Size = new System.Drawing.Size(75, 23);
+            this.button_right.TabIndex = 1;
+            this.button_right.Text = "höger";
+            this.button_right.UseVisualStyleBackColor = true;
+            this.button_right.Click += new System.EventHandler(this.button_right_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_right);
+            this.Controls.Add(this.button_left);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_left;
+        private System.Windows.Forms.Button button_right;
+    }
+}
+
---
+++ Grafiska_program/uppgift_8_1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uppgift_8_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int x_coord = 180;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            SolidBrush brush = new SolidBrush(Color.FromArgb(0, 0, 0));
+
+            g.FillRectangle(brush, x_coord, 165, 170, 80);
+        }
+
+        private void button_right_Click(object sender, EventArgs e)
+        {
+            x_coord += 5;
+            Invalidate();
+        }
+
+        private void button_left_Click(object sender, EventArgs e)
+        {
+            x_coord -= 5;
+            Invalidate();
+        }
+    }
+}
